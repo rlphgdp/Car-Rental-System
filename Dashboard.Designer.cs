526a21576ad3fd0b8edbdf97e38816b5c3f85df8
AkiraC0de <ravenakira22@gmail.com>
2026-01-16 01:59:59 +0800

    Finished the Aside Menu Bar Buttons

---
+++ Dashboard.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace VehicleManagementSystem {
+    partial class Dashboard {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.reportAndAnalyticsBtn = new FontAwesome.Sharp.IconButton();
+            this.docuManagementBtn = new FontAwesome.Sharp.IconButton();
+            this.damageAndInspecBtn = new FontAwesome.Sharp.IconButton();
+            this.maintenanceMangementBtn = new FontAwesome.Sharp.IconButton();
+            this.vehManagementBtn = new FontAwesome.Sharp.IconButton();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(54)))), ((int)(((byte)(61)))));
+            this.panelMenu.Controls.Add(this.reportAndAnalyticsBtn);
+            this.panelMenu.Controls.Add(this.docuManagementBtn);
+            this.panelMenu.Controls.Add(this.damageAndInspecBtn);
+            this.panelMenu.Controls.Add(this.maintenanceMangementBtn);
+            this.panelMenu.Controls.Add(this.vehManagementBtn);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(355, 809);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // reportAndAnalyticsBtn
+            // 
+            this.reportAndAnalyticsBtn.BackColor = System.Drawing.Color.Transparent;
+            this.reportAndAnalyticsBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.reportAndAnalyticsBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.reportAndAnalyticsBtn.FlatAppearance.BorderSize = 0;
+            this.reportAndAnalyticsBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(74)))), ((int)(((byte)(81)))));
+            this.reportAndAnalyticsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.reportAndAnalyticsBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reportAndAnalyticsBtn.ForeColor = System.Drawing.Color.White;
+            this.reportAndAnalyticsBtn.IconChar = FontAwesome.Sharp.IconChar.CircleExclamation;
+            this.reportAndAnalyticsBtn.IconColor = System.Drawing.Color.White;
+            this.reportAndAnalyticsBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.reportAndAnalyticsBtn.IconSize = 50;
+            this.reportAndAnalyticsBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.reportAndAnalyticsBtn.Location = new System.Drawing.Point(0, 523);
+            this.reportAndAnalyticsBtn.Name = "reportAndAnalyticsBtn";
+            this.reportAndAnalyticsBtn.Padding = new System.Windows.Forms.Padding(20, 0, 20, 0);
+            this.reportAndAnalyticsBtn.Size = new System.Drawing.Size(355, 85);
+            this.reportAndAnalyticsBtn.TabIndex = 5;
+            this.reportAndAnalyticsBtn.Text = "Reports and Analytic";
+            this.reportAndAnalyticsBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.reportAndAnalyticsBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.reportAndAnalyticsBtn.UseVisualStyleBackColor = false;
+            // 
+            // docuManagementBtn
+            // 
+            this.docuManagementBtn.BackColor = System.Drawing.Color.Transparent;
+            this.docuManagementBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.docuManagementBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.docuManagementBtn.FlatAppearance.BorderSize = 0;
+            this.docuManagementBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(74)))), ((int)(((byte)(81)))));
+            this.docuManagementBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.docuManagementBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.docuManagementBtn.ForeColor = System.Drawing.Color.White;
+            this.docuManagementBtn.IconChar = FontAwesome.Sharp.IconChar.FileText;
+            this.docuManagementBtn.IconColor = System.Drawing.Color.White;
+            this.docuManagementBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.docuManagementBtn.IconSize = 50;
+            this.docuManagementBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.docuManagementBtn.Location = new System.Drawing.Point(0, 438);
+            this.docuManagementBtn.Name = "docuManagementBtn";
+            this.docuManagementBtn.Padding = new System.Windows.Forms.Padding(20, 0, 20, 0);
+            this.docuManagementBtn.Size = new System.Drawing.Size(355, 85);
+            this.docuManagementBtn.TabIndex = 4;
+            this.docuManagementBtn.Text = "Document Management";
+            this.docuManagementBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.docuManagementBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.docuManagementBtn.UseVisualStyleBackColor = false;
+            // 
+            // damageAndInspecBtn
+            // 
+            this.damageAndInspecBtn.BackColor = System.Drawing.Color.Transparent;
+            this.damageAndInspecBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.damageAndInspecBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.damageAndInspecBtn.FlatAppearance.BorderSize = 0;
+            this.damageAndInspecBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(74)))), ((int)(((byte)(81)))));
+            this.damageAndInspecBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.damageAndInspecBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.damageAndInspecBtn.ForeColor = System.Drawing.Color.White;
+            this.damageAndInspecBtn.IconChar = FontAwesome.Sharp.IconChar.CarCrash;
+            this.damageAndInspecBtn.IconColor = System.Drawing.Color.White;
+            this.damageAndInspecBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.damageAndInspecBtn.IconSize = 50;
+            this.damageAndInspecBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.damageAndInspecBtn.Location = new System.Drawing.Point(0, 353);
+            this.damageAndInspecBtn.Name = "damageAndInspecBtn";
+            this.damageAndInspecBtn.Padding = new System.Windows.Forms.Padding(20, 0, 20, 0);
+            this.damageAndInspecBtn.Size = new System.Drawing.Size(355, 85);
+            this.damageAndInspecBtn.TabIndex = 3;
+            this.damageAndInspecBtn.Text = "Damage and Inspection";
+            this.damageAndInspecBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.damageAndInspecBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.damageAndInspecBtn.UseVisualStyleBackColor = false;
+            // 
+            // maintenanceMangementBtn
+            // 
+            this.maintenanceMangementBtn.BackColor = System.Drawing.Color.Transparent;
+            this.maintenanceMangementBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.maintenanceMangementBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.maintenanceMangementBtn.FlatAppearance.BorderSize = 0;
+            this.maintenanceMangementBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(74)))), ((int)(((byte)(81)))));
+            this.maintenanceMangementBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.maintenanceMangementBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maintenanceMangementBtn.ForeColor = System.Drawing.Color.White;
+            this.maintenanceMangementBtn.IconChar = FontAwesome.Sharp.IconChar.Wrench;
+            this.maintenanceMangementBtn.IconColor = System.Drawing.Color.White;
+            this.maintenanceMangementBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.maintenanceMangementBtn.IconSize = 50;
+            this.maintenanceMangementBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.maintenanceMangementBtn.Location = new System.Drawing.Point(0, 268);
+            this.maintenanceMangementBtn.Name = "maintenanceMangementBtn";
+            this.maintenanceMangementBtn.Padding = new System.Windows.Forms.Padding(20, 0, 20, 0);
+            this.maintenanceMangementBtn.Size = new System.Drawing.Size(355, 85);
+            this.maintenanceMangementBtn.TabIndex = 2;
+            this.maintenanceMangementBtn.Text = "Maintenance Management";
+            this.maintenanceMangementBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.maintenanceMangementBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.maintenanceMangementBtn.UseVisualStyleBackColor = false;
+            // 
+            // vehManagementBtn
+            // 
+            this.vehManagementBtn.BackColor = System.Drawing.Color.Transparent;
+            this.vehManagementBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.vehManagementBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.vehManagementBtn.FlatAppearance.BorderSize = 0;
+            this.vehManagementBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(74)))), ((int)(((byte)(81)))));
+            this.vehManagementBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.vehManagementBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vehManagementBtn.ForeColor = System.Drawing.Color.White;
+            this.vehManagementBtn.IconChar = FontAwesome.Sharp.IconChar.Car;
+            this.vehManagementBtn.IconColor = System.Drawing.Color.White;
+            this.vehManagementBtn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.vehManagementBtn.IconSize = 50;
+            this.vehManagementBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.vehManagementBtn.Location = new System.Drawing.Point(0, 183);
+            this.vehManagementBtn.Name = "vehManagementBtn";
+            this.vehManagementBtn.Padding = new System.Windows.Forms.Padding(20, 0, 20, 0);
+            this.vehManagementBtn.Size = new System.Drawing.Size(355, 85);
+            this.vehManagementBtn.TabIndex = 1;
+            this.vehManagementBtn.Text = "Vehicle Management";
+            this.vehManagementBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.vehManagementBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.vehManagementBtn.UseVisualStyleBackColor = false;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(355, 183);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1620, 809);
+            this.Controls.Add(this.panelMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Dashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dashboard";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.panelMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panelLogo;
+        private FontAwesome.Sharp.IconButton vehManagementBtn;
+        private FontAwesome.Sharp.IconButton reportAndAnalyticsBtn;
+        private FontAwesome.Sharp.IconButton docuManagementBtn;
+        private FontAwesome.Sharp.IconButton damageAndInspecBtn;
+        private FontAwesome.Sharp.IconButton maintenanceMangementBtn;
+    }
+}
